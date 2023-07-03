5d8f0e442c052cfcb82a7d4b61320dd7e7ba4c4e
ben-vegaprotocol <ben@vega.xyz>
2023-07-03 11:54:40 +0100

    chore:PR comments

--- tests/order_match/test_order_match.py
+++ tests/order_match/test_order_match.py
@@ -19,95 +19,22 @@ wallets = [MM_WALLET, MM_WALLET2, TRADER_WALLET,
            RANDOM_WALLET, TERMINATE_WALLET, USER_WALLET]
 
 
-def create_keys(vega):
-    for wallet in wallets:
-        vega.create_key(wallet.name)
-
-
-def mint_amount(vega, wallet_name, asset, amount):
-    vega.mint(wallet_name, asset=asset, amount=amount)
-
-
-def update_network_parameter(vega, wallet_name, parameter, new_value):
-    vega.update_network_parameter(
-        wallet_name, parameter=parameter, new_value=new_value)
-
-
-def verify_order_in_book(page, price_id):
-    bid_bar_element = page.query_selector('[data-testid="bid-bar"]')
-
-    if bid_bar_element:
-        price_element = bid_bar_element.query_selector(
-            f'[data-testid="{price_id}"]')
-
-        if price_element:
-            expect(price_element).to_be_visible()
-
-
-def manage_vega_wallet(page, wallet_name):
-    page.query_selector('[data-testid="manage-vega-wallet"]').click()
-    page.query_selector(
-        f'[data-testid="keypair-list"] div[role="menuitemradio"]:has-text("{wallet_name}")').click()
-    page.query_selector('[data-testid="manage-vega-wallet"]').click(force=True)
-
-
-def submit_order(vega, wallet, market_id, side, volume, price):
-    vega.submit_order(
-        trading_key=wallet.name,
-        market_id=market_id,
-        time_in_force="TIME_IN_FORCE_GTC",
-        order_type="TYPE_LIMIT",
-        side=side,
-        volume=volume,
-        price=price,
-    )
-
-
-def navigate_to_market(page, vega, market_name, market_id):
-    page.goto(f"http://localhost:{vega.console_port}/#/markets/{market_id}")
-    result = page.get_by_text(market_name)
-    result.first.click()
-
-
-def check_order_hidden(page, price):
-    page.query_selector('[data-testid="Orderbook"]').click()
-    bid_bar_element = page.query_selector('[data-testid="bid-bar"]')
-
-    if bid_bar_element:
-        price_element = bid_bar_element.query_selector(
-            f'[data-testid="price-{price}"]')
-
-        if price_element:
-            # Assert Order should not be displayed on the book
-            expect(price_element).to_be_hidden()
-
-
-def verify_market_data(page, data_test_id, expected_pattern):
-    page.get_by_test_id(data_test_id).click()
-    row = page.query_selector(
-        f'[data-testid="{data_test_id}"] .ag-center-cols-container .ag-row')
-
-    if row:
-        actual_text = row.evaluate("""
-            element => Array.from(element.children).filter(e => e.tagName === 'DIV').map(e => e.textContent.trim()).join(' ')
-        """)
-        print(
-            f"{data_test_id}: Expected Text: {expected_pattern} actual Text: {actual_text}")
-        assert re.fullmatch(
-            expected_pattern, actual_text), f"{data_test_id} Expected match to pattern: '{expected_pattern}', but got: '{actual_text}'"
-
-
-@pytest.mark.usefixtures("auth")
-def test_order_match2(vega, page):
+def setup_continuous_market(vega, page):
     market_name = "BTC:DAI_Mar22"
     logging.basicConfig(level=logging.INFO)
 
-    create_keys(vega)
+    for wallet in wallets:
+        vega.create_key(wallet.name)
 
-    mint_amount(vega, MM_WALLET.name, "VOTE", 1e4)
+    vega.mint(
+        MM_WALLET.name,
+        asset="VOTE",
+        amount=1e4,
+    )
 
-    update_network_parameter(vega, MM_WALLET.name,
-                             "market.fee.factors.makerFee", "0.1")
+    vega.update_network_parameter(
+        MM_WALLET.name, parameter="market.fee.factors.makerFee", new_value="0.1"
+    )
 
     vega.forward("10s")
     vega.wait_for_total_catchup()
@@ -119,9 +46,21 @@ def test_order_match2(vega, page):
     tdai_id = vega.find_asset_id(symbol="tDAI")
     print("TDAI: ", tdai_id)
 
-    mint_amount(vega, MM_WALLET.name, tdai_id, 100e5)
-    mint_amount(vega, MM_WALLET2.name, tdai_id, 100e5)
-    mint_amount(vega, USER_WALLET.name, tdai_id, 100e5)
+    vega.mint(
+        MM_WALLET.name,
+        asset=tdai_id,
+        amount=100e5,
+    )
+    vega.mint(
+        MM_WALLET2.name,
+        asset=tdai_id,
+        amount=100e5,
+    )
+    vega.mint(
+        USER_WALLET.name,
+        asset=tdai_id,
+        amount=100e5,
+    )
 
     vega.wait_fn(10)
     vega.wait_for_total_catchup()
@@ -154,31 +93,103 @@ def test_order_match2(vega, page):
     submit_order(vega, MM_WALLET, market_id, "SIDE_SELL", 1, 105)
     submit_order(vega, MM_WALLET2, market_id, "SIDE_BUY", 1, 95)
 
-    navigate_to_market(page, vega, market_name, market_id)
-
     vega.wait_for_total_catchup()
     vega.forward("10s")
 
-    submit_order(vega, USER_WALLET, market_id, "SIDE_BUY", 1, 110)
+    page.goto(f"http://localhost:{vega.console_port}/#/markets/{market_id}")
 
-    verify_order_in_book(page, 'price-11000000')
 
-    manage_vega_wallet(page, 'MarketSim')
 
-    verify_market_data(
-        page, "Open", r"BTC:DAI_Mar22 \+1 Limit Active 0\/1 110\.00 Good 'til Cancelled \(GTC\) 5\/9\/2023, \d{1,2}:\d{2}:\d{2} AM -")
+def submit_order(vega, wallet, market_id, side, volume, price):
+    vega.submit_order(
+        trading_key=wallet.name,
+        market_id=market_id,
+        time_in_force="TIME_IN_FORCE_GTC",
+        order_type="TYPE_LIMIT",
+        side=side,
+        volume=volume,
+        price=price,
+    )
 
+
+def verify_market_data(page, data_test_id, expected_pattern):
+    page.get_by_test_id(data_test_id).click()
+
+    actual_text = page.query_selector('.ag-center-cols-container').inner_text()
+    lines = actual_text.strip().split('\n')
+    for expected, actual in zip(expected_pattern, lines):
+        if re.match(r'^\\d', expected):  # check if it's a regex
+            if re.search(expected, actual):
+                print(f"Matched: {expected} == {actual}")
+            else:
+                print(f"Not Matched: {expected} != {actual}")
+        else:  # it's not a regex, so we escape it
+            if re.search(re.escape(expected), actual):
+                print(f"Matched: {expected} == {actual}")
+            else:
+                print(f"Not Matched: {expected} != {actual}")
+
+@pytest.mark.usefixtures("auth")
+def test_limit_order_trade(vega, page):
+    # setup continuous trading market with one user buy trade
+    setup_continuous_market(vega, page)
+    # Assert that the user order is displayed on the orderbook
+    orderbook_trade = page.get_by_test_id('price-11000000').nth(1)
+    #orderbook_trade[1].click()
+    expect(orderbook_trade).to_be_visible()
+
+    # Swap to user wallet
+    page.get_by_test_id('manage-vega-wallet').click(force=True)
+    page.get_by_role("menuitemradio").filter(has_text="MarketSim").click()
+    page.get_by_test_id('manage-vega-wallet').click(force=True)
+    # Assert that open position exists
+
+    expected_open_position = [
+        'BTC:DAI_Mar22',
+        '+1',
+        'Limit',
+        'Active',
+        '0/1',
+        '110.00',
+        "Good 'til Cancelled (GTC)",
+        r'\d{1,2}/\d{1,2}/\d{4},\s*\d{1,2}:\d{2}:\d{2}\s*(?:AM|PM)',
+        '-'
+    ]   
+    verify_market_data(page, "Open", expected_open_position)
+    
     vega.wait_for_total_catchup()
     vega.forward("10s")
 
-    verify_market_data(
-        page, "Trades", r"107.50 1 5/9/2023, \d{1,2}:\d{2}:\d{2} AM")
-    verify_market_data(
-        page, "Positions", 'BTC:DAI_Mar22 107.50 +1 - 0.00 tDAI 107.50 0.0 8.50269 0.00 0.00')
-    check_order_hidden(page, 11000000)
+    print("Assert Position:")
+    # Assert that Position exists
+    expected_position = [
+        'BTC:DAI_Mar22',
+        '107.50',
+        '+1',
+        '-',
+        '0.00',
+        'tDAI',
+        '107.50',
+        '0.0',
+        '8.50269',
+        '0.00',
+        '0.00'
+    ]
+    verify_market_data(page, "Positions", expected_position )
+    
+    print("Assert Trades:")
+    # Assert that trade exists
+    expected_trade = [
+        '107.50',
+        '1',
+        r'\d{1,2}/\d{1,2}/\d{4},\s*\d{1,2}:\d{2}:\d{2}\s*(?:AM|PM)'
+    ]
+    verify_market_data(page, "Trades", expected_trade)
 
-    print("END")
-
+    # Assert that the order is no longer on the orderbook
+    page.get_by_test_id('Orderbook').click()
+    price_element = page.get_by_test_id('price-11000000').nth(1)
+    expect(price_element).to_be_hidden()
 
 if __name__ == "__main__":
     pytest.main([__file__])

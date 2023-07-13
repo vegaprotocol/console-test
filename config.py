06aa3ce87fda4efbcbd3f5e633790a9f777b62a1
Ben Wallace <ben@vega.xyz>
2023-07-13 14:27:41 +0100

    test(trading): initial commit iceberg orders

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # replace this with local docker image of latest develop branch of frontend-monorepo
 # for test_basic to work
-container_name = "vegaprotocol/trading:testnet"
+container_name = "ghcr.io/vegaprotocol/frontend/trading:70ca29c9ee77c53211eeb7fddf6ad4162682a382"
+ #TODO: will need to change this once iceberg pr merged.

b25bc82b51dbc2de1d163fa8fc3c4cb9945b926b
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-07-24 14:54:57 +0200

    add comment

--- tests/market/test_market.py
+++ tests/market/test_market.py
@@ -71,6 +71,8 @@ def test_open_market(setup_simple_market, vega:VegaService, page: Page):
 
     expect(page.get_by_test_id(liquidity_supplied
                                ).get_by_test_id(item_value)).to_have_text("0.00 (0.00%)")
+
+    # add orders to provide liquidity
     submit_order(vega, MM_WALLET.name, market_id, "SIDE_BUY",
                  initial_volume, initial_price)
     submit_order(vega, MM_WALLET.name, market_id, "SIDE_SELL",

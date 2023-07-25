65ecd58379c688a50b6c9a97b2f378950f0caf35
Matthew Russell <mattrussell36@gmail.com>
2023-07-25 18:26:50 +0100

    chore: revert container id

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 # replace this with local docker image of latest develop branch of frontend-monorepo
 # for test_basic to work
-# container_name = "vegaprotocol/trading:testnet"
-container_name = "ghcr.io/vegaprotocol/frontend/trading:00bdcd6cf8aa33ee7ec52566f791dbbff656e1b6"
+container_name = "vegaprotocol/trading:testnet"

215a0d4e4c59796fa347c287e22bb1d99bb06cf6
Rado <szpiechrados@gmail.com>
2023-08-02 11:28:06 +0200

    chore: change docker image

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # replace this with local docker image of latest develop branch of frontend-monorepo
 # for test_basic to work
-container_name = "vegaprotocol/trading:aba81e85c2e903c9ed3ac9007bcc02de2b90184a"
+container_name = "vegaprotocol/trading:develop"

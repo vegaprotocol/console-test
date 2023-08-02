479c7402876024412e2afd7a52bdab3a32a930f5
bwallacee <ben@vega.xyz>
2023-08-02 16:26:06 +0100

    feat(trading): fix setting tests

--- tests/deal_ticket/test_fees_margin_estimations.py
+++ tests/deal_ticket/test_fees_margin_estimations.py
@@ -127,7 +127,9 @@ def test_margin_and_fees_estimations(vega: VegaService, page: Page):
     # submit order from UI and verify fees and margin
     expect(page.get_by_test_id(notional)).to_have_text("Notional- BTC")
     expect(page.get_by_test_id(fees)).to_have_text("Fees- tDAI")
-    expect(page.get_by_test_id(margin_required)).to_have_text("Margin required0.00 tDAI")
+    expect(page.get_by_test_id(margin_required)).to_have_text(
+        "Margin required0.00 tDAI"
+    )
     page.get_by_test_id("order-size").type("200")
     page.get_by_test_id("order-price").type("20")
 
@@ -136,39 +138,54 @@ def test_margin_and_fees_estimations(vega: VegaService, page: Page):
 
     expect(page.get_by_test_id(notional)).to_have_text("Notional4,000.00 BTC")
     expect(page.get_by_test_id(fees)).to_have_text("Fees~402.00 tDAI")
-    expect(page.get_by_test_id(margin_required)).to_have_text("Margin required1,661.72707 - 1,661.88832 tDAI")
-    
+    expect(page.get_by_test_id(margin_required)).to_have_text(
+        "Margin required1,661.72707 - 1,661.88832 tDAI"
+    )
 
     page.get_by_test_id("place-order").click()
 
     vega.wait_fn(1)
     vega.wait_for_total_catchup()
 
-    expect(page.get_by_test_id(margin_required)).to_have_text("Margin required1,661.72707 - 1,661.88832 tDAI ")
+    expect(page.get_by_test_id(margin_required)).to_have_text(
+        "Margin required1,661.72707 - 1,661.88832 tDAI "
+    )
     page.get_by_test_id("toast-close").click()
-    
+
     # submit order by sim function
     order = submit_order(vega, "Key 1", market_id, "SIDE_BUY", 400, 38329483272398.838)
-    vega.wait_for_total_catchup() 
     vega.forward("10s")
-    expect(page.get_by_test_id(margin_required)).to_have_text("Margin required897,716,007,278,879.50 - 897,716,007,278,895.20 tDAI ")
-    expect(page.get_by_test_id("deal-ticket-warning-margin")).to_contain_text("You may not have enough margin available to open this position.") 
+    vega.wait_for_total_catchup()
+    expect(page.get_by_test_id(margin_required)).to_have_text(
+        "Margin required897,716,007,278,879.50 - 897,716,007,278,895.20 tDAI "
+    )
+    expect(page.get_by_test_id("deal-ticket-warning-margin")).to_contain_text(
+        "You may not have enough margin available to open this position."
+    )
 
-    # cancel order and verify that warning margin disappeared 
+    # cancel order and verify that warning margin disappeared
     vega.cancel_order("Key 1", market_id, order)
-    vega.wait_for_total_catchup() 
+    vega.wait_for_total_catchup()
     vega.forward("10s")
-    expect(page.get_by_test_id("deal-ticket-warning-auction")).to_contain_text("Any orders placed now will not trade until the auction ends") 
-    
+    expect(page.get_by_test_id("deal-ticket-warning-auction")).to_contain_text(
+        "Any orders placed now will not trade until the auction ends"
+    )
+
     # add order at the current price so that it is possible to change the status to price monitoring
     submit_order(vega, "Key 1", market_id, "SIDE_SELL", 1, 110)
-    vega.wait_for_total_catchup() 
+    vega.wait_for_total_catchup()
     vega.forward("10s")
-    expect(page.get_by_test_id(margin_required)).to_have_text("Margin required1,684.36688 - 1,700.53688 tDAI")
-    expect(page.get_by_test_id(market_trading_mode).get_by_test_id(item_value)).to_have_text("Continuous")
-    
-    #verify if we can submit order after reverted margin
+    expect(page.get_by_test_id(margin_required)).to_have_text(
+        "Margin required1,684.36688 - 1,700.53688 tDAI"
+    )
+    expect(
+        page.get_by_test_id(market_trading_mode).get_by_test_id(item_value)
+    ).to_have_text("Continuous")
+
+    # verify if we can submit order after reverted margin
     page.get_by_test_id("place-order").click()
     vega.wait_fn(1)
     vega.wait_for_total_catchup()
-    expect(page.get_by_test_id("toast-content")).to_contain_text("Your transaction has been confirmed") 
+    expect(page.get_by_test_id("toast-content")).to_contain_text(
+        "Your transaction has been confirmed"
+    )
--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -1,17 +1,32 @@
 
 import pytest
-from playwright.sync_api import expect
+from playwright.sync_api import expect, Page
+from vega_sim.service import VegaService
+import time
+import requests
+
+def wait_for_service(url, timeout=60):
+    start_time = time.time()
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                break
+        except Exception:
+            # If the connection attempt fails, wait and try again
+            time.sleep(1)
+            if time.time() - start_time > timeout:
+                raise TimeoutError(f"Timed out waiting for service at {url}")
 
 
-@pytest.fixture()
-def setup(vega, page):
-    page.goto(f"http://localhost:{vega.console_port}/")
-    page.get_by_test_id('dialog-close').click()
-    page.click('[aria-label="cog icon"]')
 
 
-@pytest.mark.usefixtures("auth", "setup")
-def test_share_usage_data(page):
+@pytest.mark.usefixtures("auth", "risk_accepted")
+def test_share_usage_data(page: Page, vega: VegaService):
+    
+    wait_for_service(f"http://localhost:{vega.console_port}/#/settings")
+    page.goto(f"http://localhost:{vega.console_port}/#/settings", timeout=30000)
+
     telemetry_checkbox = page.locator('#telemetry-approval')
     expect(telemetry_checkbox).to_have_attribute('data-state', 'unchecked')
 
@@ -47,9 +62,10 @@ ICON_TO_TOAST = {
         'class="group absolute z-20 bottom-0 right-0 p-[8px_16px_16px_16px] max-w-full max-h-full overflow-x-hidden overflow-y-auto"',
 }
 
-
-@pytest.mark.usefixtures("auth", "setup")
-def test_toast_positions(page):
+@pytest.mark.usefixtures("auth", "risk_accepted")
+def test_toast_positions(page: Page, vega: VegaService):
+    wait_for_service(f"http://localhost:{vega.console_port}/#/settings")
+    page.goto(f"http://localhost:{vega.console_port}/#/settings", timeout=30000)
     for icon_selector, toast_selector in ICON_TO_TOAST.items():
         # Click the icon
         page.click(f"[{icon_selector}]")
@@ -57,8 +73,10 @@ def test_toast_positions(page):
         expect(page.locator(f"[{toast_selector}]")).to_be_visible()
 
 
-@pytest.mark.usefixtures("auth", "setup")
-def test_dark_mode(page):
+@pytest.mark.usefixtures("auth", "risk_accepted")
+def test_dark_mode(page: Page, vega: VegaService):
+    wait_for_service(f"http://localhost:{vega.console_port}/#/settings")
+    page.goto(f"http://localhost:{vega.console_port}/#/settings")
     assert page.query_selector('html').get_attribute('class') == None
     page.get_by_role('switch').click()
     assert page.query_selector('html').get_attribute('class') == "dark"

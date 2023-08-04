68b2d288bba7905c12eb29f5259e84863a9161cc
bwallacee <ben@vega.xyz>
2023-08-04 15:43:01 +0100

    feat(trading): get started tests

--- tests/get_started/test_get_started.py
+++ tests/get_started/test_get_started.py
@@ -1,29 +1,30 @@
 import pytest
 from playwright.sync_api import expect, Page
-from vega_sim.service import VegaService
 from playwright.sync_api import expect
 
+@pytest.mark.usefixtures("simple_market")
+def test_get_started_dialog(page: Page):
 
-def test_get_started_dialog(vega:VegaService, page: Page):
-    page.goto(f"http://localhost:{vega.console_port}/#/disclaimer", wait_until='domcontentloaded')
-    
+    page.goto(f"/#/disclaimer")
+
     expect(page.get_by_test_id('welcome-dialog')).to_be_visible()
     expect(page.get_by_test_id('get-started-button')).to_be_visible()
     page.get_by_test_id('get-started-button').click()
     expect(page.get_by_test_id('connector-jsonRpc')).to_be_visible()
     
-@pytest.mark.usefixtures("risk_accepted")
-def test_get_started_seen_already(vega:VegaService,page: Page):
-    page.goto(f"http://localhost:{vega.console_port}/#/disclaimer", wait_until='domcontentloaded')
+@pytest.mark.usefixtures("simple_market","risk_accepted")
+def test_get_started_seen_already(simple_market, page: Page):
+    page.goto(f"/#/markets/{simple_market}")
     locator = page.get_by_test_id('connect-vega-wallet')
     page.wait_for_selector('[data-testid="connect-vega-wallet"]', state='attached')
     expect(locator).to_be_enabled
     expect(locator).to_be_visible
     expect(locator).to_have_text('Get started')
 
-def test_browser_wallet_installed(vega:VegaService,page: Page):
+@pytest.mark.usefixtures("simple_market")
+def test_browser_wallet_installed(simple_market, page: Page):
     page.add_init_script("window.vega = {}")
-    page.goto(f"http://localhost:{vega.console_port}/#/disclaimer", wait_until='domcontentloaded')
+    page.goto(f"/#/markets/{simple_market}")
     locator = page.get_by_test_id('connect-vega-wallet')
     page.wait_for_selector('[data-testid="connect-vega-wallet"]', state='attached')
     expect(locator).to_be_enabled

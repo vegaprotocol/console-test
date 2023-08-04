26b3d696777607374919afa85d0fd0cbe4848846
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-08-04 17:29:22 +0200

    add acs

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # replace this with local docker image of latest develop branch of frontend-monorepo
 # for test_basic to work
-container_name = "ghcr.io/vegaprotocol/frontend/trading:6ffb584e8a705038ccebccada3f119c38f6e67b9"
+container_name = "vegaprotocol/trading:develop"
--- tests/deal_ticket/test_stop_order.py
+++ tests/deal_ticket/test_stop_order.py
@@ -48,17 +48,21 @@ updatedAt_col = '[col-id="updatedAt"]'
 @pytest.mark.usefixtures("continuous_market", "auth")
 def test_stop_order_form_error_validation(continuous_market, vega: VegaService, page: Page):
 
+    # 7002-SORD-032 
+
     market_id = continuous_market
     page.goto(f"/#/markets/{market_id}")
-
     page.get_by_test_id(stop_order_btn).click()
-    page.get_by_test_id(stop_market_order_btn).is_visible()
-    page.get_by_test_id(stop_market_order_btn).click()
+    page.get_by_test_id(stop_limit_order_btn).is_visible()
+    page.get_by_test_id(stop_limit_order_btn).click()
     page.get_by_test_id(order_side_sell).click()
     page.get_by_test_id(submit_stop_order).click()
-    expect(page.get_by_test_id("stop-order-error-message-trigger-price")).to_be_visible()
-    expect(page.get_by_test_id("stop-order-error-message-size")).to_be_visible()
-
+    expect(page.get_by_test_id("stop-order-error-message-trigger-price")).to_have_text("You need provide a price")
+    expect(page.get_by_test_id("stop-order-error-message-size")).to_have_text("Size cannot be lower than 1")
+    
+    page.get_by_test_id(order_size).fill("1")
+    page.get_by_test_id(order_price).fill("0.0000001")
+    expect(page.get_by_test_id("stop-order-error-message-price")).to_have_text("Price cannot be lower than 0.00001")
 
 @pytest.mark.usefixtures("continuous_market", "auth")
 def test_submit_stop_order_rejected(continuous_market, vega: VegaService, page: Page):
@@ -92,6 +96,11 @@ def test_submit_stop_order_rejected(continuous_market, vega: VegaService, page: 
 
 @pytest.mark.usefixtures("continuous_market", "auth")
 def test_submit_stop_market_order_triggered(continuous_market, vega: VegaService, page: Page):
+    # 7002-SORD-071
+    # 7002-SORD-074
+    # 7002-SORD-075
+    # 7002-SORD-068
+
 
     market_id = continuous_market
     page.goto(f"/#/markets/{market_id}")
@@ -109,7 +118,7 @@ def test_submit_stop_market_order_triggered(continuous_market, vega: VegaService
     page.get_by_test_id(order_size).type("1")
     page.get_by_test_id(expire).click()
     expires_at = datetime.now() + timedelta(days=1)
-    expires_at_input_value = expires_at.strftime('%d-%Y-%mT%H:%M')
+    expires_at_input_value = expires_at.strftime('%Y-%m-%dT%H:%M:%S')
     page.get_by_test_id('date-picker-field').type(expires_at_input_value)
     page.get_by_test_id(expiry_strategy_cancel).click()
 
@@ -133,7 +142,11 @@ def test_submit_stop_market_order_triggered(continuous_market, vega: VegaService
 
 @pytest.mark.usefixtures("continuous_market", "auth")
 def test_submit_stop_limit_order_pending(continuous_market, vega: VegaService, page: Page):
-   
+    # 7002-SORD-071
+    # 7002-SORD-074
+    # 7002-SORD-075
+    # 7002-SORD-069
+
     market_id = continuous_market
     page.goto(f"/#/markets/{market_id}")
 
@@ -150,10 +163,11 @@ def test_submit_stop_limit_order_pending(continuous_market, vega: VegaService, p
     page.get_by_test_id(trigger_price).type("102")
     page.get_by_test_id(order_price).type("99")
     page.get_by_test_id(order_size).type("1")
+    page.get_by_test_id("order-tif").select_option("TIME_IN_FORCE_IOC")
     page.get_by_test_id(expire).click()
     expires_at = datetime.now() + timedelta(days=1)
-    expires_at_input_value = expires_at.strftime('%d-%Y-%mT%H:%M')
-    page.get_by_test_id('date-picker-field').type(expires_at_input_value)
+    expires_at_input_value = expires_at.strftime('%Y-%m-%dT%H:%M:%S')
+    page.get_by_test_id('date-picker-field').fill(expires_at_input_value)
     page.get_by_test_id(submit_stop_order).click()
     page.get_by_test_id(stop_orders_tab).click()
 
@@ -161,7 +175,6 @@ def test_submit_stop_limit_order_pending(continuous_market, vega: VegaService, p
     vega.forward("10s")
     vega.wait_for_total_catchup()
     page.get_by_test_id("toast-close").click()
-
     expect((page.get_by_role(row_table).locator(market_name_col)).nth(1)).to_have_text("BTC:DAI_2023")
     expect((page.get_by_role(row_table).locator(trigger_col)).nth(1)).to_have_text("Mark < 102.00")
     expect((page.get_by_role(row_table).locator(expiresAt_col)).nth(1)).to_have_text("")
@@ -169,7 +182,7 @@ def test_submit_stop_limit_order_pending(continuous_market, vega: VegaService, p
     expect((page.get_by_role(row_table).locator(submission_type)).nth(1)).to_have_text("Limit")
     expect((page.get_by_role(row_table).locator(status_col)).nth(1)).to_have_text("Pending")
     expect((page.get_by_role(row_table).locator(price_col)).nth(1)).to_have_text("99.00")
-    expect((page.get_by_role(row_table).locator(timeInForce_col)).nth(1)).to_have_text("FOK")
+    expect((page.get_by_role(row_table).locator(timeInForce_col)).nth(1)).to_have_text("IOC")
     expect((page.get_by_role(row_table).locator(updatedAt_col)).nth(1)).not_to_be_empty()
 
 @pytest.mark.usefixtures("continuous_market", "auth")
@@ -210,11 +223,19 @@ def test_submit_stop_limit_order_cancel(continuous_market, vega: VegaService, pa
     expect((page.get_by_role(row_table).locator('[col-id="status"]')).nth(1)).to_have_text("Cancelled")
     
 @pytest.mark.usefixtures("continuous_market", "auth")
-def test_stop_order_form_validation(continuous_market, vega: VegaService, page: Page):
+def test_stop_market_order_form_validation(continuous_market, vega: VegaService, page: Page):
+
+    # 7002-SORD-052
+    # 7002-SORD-055
+    # 7002-SORD-056
+    # 7002-SORD-057
+    # 7002-SORD-058
+    # 7002-SORD-064
+    # 7002-SORD-065
 
     market_id = continuous_market
     page.goto(f"/#/markets/{market_id}")
-
+    
     page.get_by_test_id(stop_order_btn).click()
     page.get_by_test_id(stop_market_order_btn).is_visible()
     page.get_by_test_id(stop_market_order_btn).click()
@@ -228,7 +249,44 @@ def test_stop_order_form_validation(continuous_market, vega: VegaService, page: 
     expect(page.locator('[for="input-price-quote"]')).to_have_text("Size")
     page.get_by_test_id(order_size).click()
     expect(page.get_by_test_id(order_size)).to_be_empty
+    expect(page.get_by_test_id(order_price)).not_to_be_visible()
   
+@pytest.mark.usefixtures("continuous_market", "auth")
+def test_stop_limit_order_form_validation(continuous_market, vega: VegaService, page: Page):
+
+    # 7002-SORD-020
+    # 7002-SORD-021
+    # 7002-SORD-022
+    # 7002-SORD-033
+    # 7002-SORD-034
+    # 7002-SORD-035
+    # 7002-SORD-036
+    # 7002-SORD-037
+    # 7002-SORD-038
+    # 7002-SORD-049
+    # 7002-SORD-050
+    # 7002-SORD-051
+
+    market_id = continuous_market
+    page.goto(f"/#/markets/{market_id}")
+    page.get_by_test_id(stop_order_btn).click()
+    page.get_by_test_id(stop_limit_order_btn).is_visible()
+    page.get_by_test_id(stop_limit_order_btn).click()
+    expect(page.get_by_test_id("sidebar-content").get_by_text("Trigger")).to_be_visible()
+    expect(page.locator('[for="triggerDirection-risesAbove"]')).to_have_text("Rises above")
+    expect(page.locator('[for="triggerDirection-risesAbove"]')).to_be_checked
+    expect(page.locator('[for="triggerDirection-fallsBelow"]')).to_have_text("Falls below")
+    page.get_by_test_id(trigger_price).click()
+    expect(page.get_by_test_id(trigger_price)).to_be_empty
+    expect(page.locator('[for="triggerType-price"]')).to_have_text("Price")
+    expect(page.locator('[for="triggerType-price"]')).to_be_checked
+    expect(page.locator('[for="triggerType-trailingPercentOffset"]')).to_have_text("Trailing Percent Offset")    
+    expect(page.locator('[for="input-price-quote"]').first).to_have_text("Size")
+    expect(page.locator('[for="input-price-quote"]').last).to_have_text("Price (BTC)")
+    page.get_by_test_id(order_size).click()
+    expect(page.get_by_test_id(order_size)).to_be_empty
+    page.get_by_test_id(order_price).click()
+    expect(page.get_by_test_id(order_price)).to_be_empty()
 
 
 

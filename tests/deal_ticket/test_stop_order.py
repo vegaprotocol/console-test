4541f02e6fcb9b7428af0dca59aed66ea0a64979
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-08-23 19:51:55 +0200

    add refresh page

--- tests/deal_ticket/test_stop_order.py
+++ tests/deal_ticket/test_stop_order.py
@@ -96,6 +96,7 @@ def test_submit_stop_order_rejected(continuous_market, vega: VegaService, page: 
     vega.wait_for_total_catchup()
     page.get_by_test_id(close_toast).click()
 
+    page.reload()
     page.locator('[ref="eOverlayWrapper"]').is_hidden()
     page.get_by_role(row_table).locator(market_name_col).nth(1).is_visible()
     expect((page.get_by_role(row_table).locator(market_name_col)).nth(1)).to_have_text(
@@ -160,6 +161,7 @@ def test_submit_stop_market_order_triggered(
     page.wait_for_selector('[data-testid="toast-close"]', state="visible")
     page.get_by_test_id(close_toast).click()
 
+    page.reload()
     page.locator('[ref="eOverlayWrapper"]').is_hidden()
     page.get_by_role(row_table).locator(market_name_col).nth(1).is_visible()
     expect((page.get_by_role(row_table).locator(market_name_col)).nth(1)).to_have_text(
@@ -224,6 +226,7 @@ def test_submit_stop_limit_order_pending(
     page.wait_for_selector('[data-testid="toast-close"]', state="visible")
     page.get_by_test_id(close_toast).click()
 
+    page.reload()
     page.locator('[ref="eOverlayWrapper"]').is_hidden()
     page.get_by_role(row_table).locator(market_name_col).nth(1).is_visible()
     expect((page.get_by_role(row_table).locator(market_name_col)).nth(1)).to_have_text(

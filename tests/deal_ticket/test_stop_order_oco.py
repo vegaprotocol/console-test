1d15dfe421c839d740667979b793d4bf919d9d86
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-08-31 11:19:10 +0200

    update ACs coverage

--- tests/deal_ticket/test_stop_order_oco.py
+++ tests/deal_ticket/test_stop_order_oco.py
@@ -94,7 +94,7 @@ def test_submit_stop_order_market_oco_rejected(continuous_market, vega: VegaServ
     page.get_by_test_id(stop_market_order_btn).click()
     page.get_by_test_id(trigger_price).fill("103")
     page.get_by_test_id(order_size).fill("3")
-    
+    # 7002-SORD-098
     expect(page.get_by_test_id("stop-order-warning-message-trigger-price")).to_have_text("Stop order will be triggered immediately")
     
     # 7002-SORD-082
@@ -246,7 +246,7 @@ def test_submit_stop_oco_market_order_pending(continuous_market, vega: VegaServi
     page.get_by_test_id(trigger_price).fill("99")
     page.get_by_test_id(order_size).fill("3")
     page.get_by_test_id(oco).click()
-    expect(page.get_by_test_id(trigger_direction_fallsBelow_oco)).to_be_checked
+    expect(page.get_by_test_id(trigger_direction_fallsAbove_oco)).to_be_checked
     page.get_by_test_id(trigger_price_oco).fill("120")
     page.get_by_test_id(order_size_oco).fill("2")
     page.pause()
@@ -283,7 +283,7 @@ def test_submit_stop_oco_limit_order_pending(continuous_market, vega: VegaServic
     page.get_by_test_id(order_price).fill("103")
     page.get_by_test_id(oco).click()
     # 7002-SORD-090
-    expect(page.get_by_test_id(trigger_direction_fallsBelow_oco)).to_be_checked
+    expect(page.get_by_test_id(trigger_direction_fallsAbove_oco)).to_be_checked
     page.get_by_test_id(trigger_price_oco).fill("120")
     page.get_by_test_id(order_size_oco).fill("2")
     # 7002-SORD-089
@@ -352,3 +352,62 @@ def test_submit_stop_oco_limit_order_cancel(continuous_market, vega: VegaService
 
     expect(page.locator(".ag-center-cols-container").locator('[col-id="status"]').first).to_have_text("CancelledOCO")
     expect(page.locator(".ag-center-cols-container").locator('[col-id="status"]').last).to_have_text("CancelledOCO")
+
+@pytest.mark.usefixtures("continuous_market", "auth", "risk_accepted")
+def test_stop_market_order_oco_form_validation(continuous_market, page: Page):
+    
+    market_id = continuous_market
+    page.goto(f"/#/markets/{market_id}")
+    page.get_by_test_id(stop_order_btn).click()
+    page.get_by_test_id(stop_market_order_btn).is_visible()
+    page.get_by_test_id(stop_market_order_btn).click()
+    page.get_by_test_id(oco).click()
+    expect(
+        page.get_by_test_id("sidebar-content").get_by_text("Trigger").last
+    ).to_be_visible()
+    # 7002-SORD-084
+    expect(page.locator('[for="triggerDirection-risesAbove-oco"]')).to_have_text(
+        "Rises above"
+    )
+    # 7002-SORD-085
+    expect(page.locator('[for="triggerDirection-fallsBelow-oco"]')).to_have_text(
+        "Falls below"
+    )
+     # 7002-SORD-087
+    expect(page.locator('[for="triggerType-price-oco"]')).to_have_text("Price")
+    expect(page.locator('[for="triggerType-price"]')).to_be_checked
+    # 7002-SORD-088
+    expect(page.locator('[for="triggerType-trailingPercentOffset-oco"]')).to_have_text(
+        "Trailing Percent Offset"
+    )
+    expect(page.locator('[for="order-size-oco"]')).to_have_text("Size")
+
+@pytest.mark.usefixtures("continuous_market", "auth", "risk_accepted")
+def test_stop_limit_order_oco_form_validation(continuous_market, page: Page):
+  
+    market_id = continuous_market
+    page.goto(f"/#/markets/{market_id}")
+    page.get_by_test_id(stop_order_btn).click()
+    page.get_by_test_id(stop_market_order_btn).is_visible()
+    page.get_by_test_id(stop_market_order_btn).click()
+    page.get_by_test_id(oco).click()
+    expect(
+        page.get_by_test_id("sidebar-content").get_by_text("Trigger").last
+    ).to_be_visible()
+     # 7002-SORD-090
+    expect(page.locator('[for="triggerDirection-risesAbove-oco"]')).to_have_text(
+        "Rises above"
+    )
+    # 7002-SORD-091
+    expect(page.locator('[for="triggerDirection-fallsBelow-oco"]')).to_have_text(
+        "Falls below"
+    )
+    # 7002-SORD-095
+    expect(page.locator('[for="triggerType-price-oco"]')).to_have_text("Price")
+    expect(page.locator('[for="triggerType-price"]')).to_be_checked
+    # 7002-SORD-095
+    expect(page.locator('[for="triggerType-trailingPercentOffset-oco"]')).to_have_text(
+        "Trailing Percent Offset"
+    )
+    expect(page.locator('[for="order-size-oco"]')).to_have_text("Size")
+    expect(page.locator('[for="order-price-oco"]').last).to_have_text("Price (BTC)")

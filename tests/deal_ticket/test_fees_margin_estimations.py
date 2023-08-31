6194419268da5b886e9f5a37aed8f309891953a4
bwallacee <ben@vega.xyz>
2023-08-31 15:28:35 +0100

    chore(trading): fix margins test

--- tests/deal_ticket/test_fees_margin_estimations.py
+++ tests/deal_ticket/test_fees_margin_estimations.py
@@ -16,7 +16,7 @@ margin_required = "deal-ticket-fee-margin-required"
 item_value = "item-value"
 market_trading_mode = "market-trading-mode"
 
-
+@pytest.mark.parametrize("vega", [120], indirect=True)
 @pytest.mark.usefixtures("page", "vega", "continuous_market", "auth", "risk_accepted")
 def test_margin_and_fees_estimations(continuous_market, vega: VegaService, page: Page):
     # setup continuous trading market with one user buy trade
@@ -32,7 +32,7 @@ def test_margin_and_fees_estimations(continuous_market, vega: VegaService, page:
     page.get_by_test_id("order-size").type("200")
     page.get_by_test_id("order-price").type("20")
 
-    vega.wait_fn(1)
+    vega.wait_fn(10)
     vega.wait_for_total_catchup()
 
     expect(page.get_by_test_id(notional)).to_have_text("Notional4,000.00 BTC")
@@ -43,9 +43,8 @@ def test_margin_and_fees_estimations(continuous_market, vega: VegaService, page:
 
     page.get_by_test_id("place-order").click()
 
-    vega.wait_fn(1)
+    vega.wait_fn(10)
     vega.wait_for_total_catchup()
-
     expect(page.get_by_test_id(margin_required)).to_have_text(
         "Margin required1,661.72707 - 1,661.88832 tDAI "
     )
@@ -53,10 +52,10 @@ def test_margin_and_fees_estimations(continuous_market, vega: VegaService, page:
 
     # submit order by sim function
     order = submit_order(vega, "Key 1", market_id, "SIDE_BUY", 400, 38329483272398.838)
-    vega.forward("10s")
+    vega.forward("20s")
     vega.wait_for_total_catchup()
     expect(page.get_by_test_id(margin_required)).to_have_text(
-        "Margin required897,716,007,278,879.40 - 897,716,007,278,895.20 tDAI "
+        "Margin required897,716,007,278,782.40 - 897,716,007,278,798.50 tDAI "
     )
     expect(page.get_by_test_id("deal-ticket-warning-margin")).to_contain_text(
         "You may not have enough margin available to open this position."
@@ -64,7 +63,8 @@ def test_margin_and_fees_estimations(continuous_market, vega: VegaService, page:
 
     # cancel order and verify that warning margin disappeared
     vega.cancel_order("Key 1", market_id, order)
-    vega.forward("10s")
+    vega.forward("20s")
+    vega.wait_fn(10)
     vega.wait_for_total_catchup()
     expect(page.get_by_test_id("deal-ticket-warning-auction")).to_contain_text(
         "Any orders placed now will not trade until the auction ends"
@@ -72,8 +72,10 @@ def test_margin_and_fees_estimations(continuous_market, vega: VegaService, page:
 
     # add order at the current price so that it is possible to change the status to price monitoring
     submit_order(vega, "Key 1", market_id, "SIDE_SELL", 1, 110)
-    vega.forward("10s")
+    vega.forward("20s")
+    vega.wait_fn(10)
     vega.wait_for_total_catchup()
+    page.reload()
     expect(page.get_by_test_id(margin_required)).to_have_text(
         "Margin required1,684.36688 - 1,700.53688 tDAI"
     )

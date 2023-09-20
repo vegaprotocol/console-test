c270b1736dd1c173e204913df7b298b1fea59c3a
Radosław Szpiech <szpiechrados@gmail.com>
2023-09-20 15:33:25 +0200

    chore: optimize market-sim usage in market tests (#127)

--- tests/market/test_markets_proposed.py
+++ tests/market/test_markets_proposed.py
@@ -7,6 +7,9 @@ from collections import namedtuple
 from playwright.sync_api import Page, expect
 from vega_sim.service import VegaService
 
+from conftest import init_vega
+from fixtures.market import setup_simple_market
+
 # Defined namedtuples
 WalletConfig = namedtuple("WalletConfig", ["name", "passphrase"])
 
@@ -21,51 +24,49 @@ row_selector = '[data-testid="tab-proposed-markets"] .ag-center-cols-container .
 col_market_id = '[col-id="market"] [data-testid="stack-cell-primary"]'
 
 
-@pytest.mark.usefixtures("proposed_market", "risk_accepted")
-def test_can_see_table_headers(proposed_market, vega: VegaService, page: Page):
+@pytest.fixture(scope="module")
+def vega(request):
+    with init_vega(request) as vega:
+        yield vega
 
-    # setup market in proposed step, without liquidity provided
-    market_id = proposed_market
-    page.goto(f"/#/markets/{market_id}")
 
+@pytest.fixture(scope="module")
+def proposed_market(vega: VegaService):
+    # setup market without liquidity provided
+    market_id = setup_simple_market(vega, approve_proposal=False)
     # approve market
     governance.approve_proposal(
         key_name=MM_WALLET.name,
         proposal_id=market_id,
         wallet=vega.wallet,
     )
-    page.goto(f"/#/markets/all")
+    return market_id
+
+
+@pytest.mark.usefixtures("risk_accepted")
+def test_can_see_table_headers(proposed_market, page: Page):
+    page.goto("/#/markets/all")
     page.click('[data-testid="Proposed markets"]')
 
     # Test that you can see table headers
     headers = [
-            'Market',
-            'Settlement asset',
-            'State',
-            'Parent market',
-            'Voting',
-            'Closing date',
-            'Enactment date',
-            '',
-        ]
-    
+        "Market",
+        "Settlement asset",
+        "State",
+        "Parent market",
+        "Voting",
+        "Closing date",
+        "Enactment date",
+        "",
+    ]
+
     header_elements = page.locator(".ag-header-cell-text")
     for i, header in enumerate(headers):
-            assert header_elements.nth(i).inner_text() == header
-    
-@pytest.mark.usefixtures("proposed_market", "risk_accepted")
-def test_renders_markets_correctly(proposed_market, vega: VegaService, page: Page):
+        assert header_elements.nth(i).inner_text() == header
 
-    # setup market in proposed step, without liquidity provided
-    market_id = proposed_market
-    page.goto(f"/#/markets/{market_id}")
 
-    # approve market
-    governance.approve_proposal(
-        key_name=MM_WALLET.name,
-        proposal_id=market_id,
-        wallet=vega.wallet,
-    )
+@pytest.mark.usefixtures("risk_accepted")
+def test_renders_markets_correctly(proposed_market, page: Page):
     page.goto(f"/#/markets/all")
     page.click('[data-testid="Proposed markets"]')
     row = page.locator(row_selector)
@@ -78,7 +79,9 @@ def test_renders_markets_correctly(proposed_market, vega: VegaService, page: Pag
     # 6001-MARK-052
     # 6001-MARK-053
     expect(row.locator('[col-id="state"]')).to_have_text("Open")
-    expect(row.locator('[col-id="terms.change.successorConfiguration.parentMarketId"]')).to_have_text("-")
+    expect(
+        row.locator('[col-id="terms.change.successorConfiguration.parentMarketId"]')
+    ).to_have_text("-")
 
     # 6001-MARK-054
     # 6001-MARK-055
@@ -88,56 +91,42 @@ def test_renders_markets_correctly(proposed_market, vega: VegaService, page: Pag
     expect(row.locator('[col-id="closing-date"]')).not_to_be_empty()
 
     # 6001-MARK-057
-    expect(row.locator('[col-id="enactment-date"]')).not_to_be_empty   
+    expect(row.locator('[col-id="enactment-date"]')).not_to_be_empty
 
     # 6001-MARK-058
     page.get_by_test_id("dropdown-menu").click()
     dropdown_content = '[data-testid="proposal-actions-content"]'
-    first_item_link = page.locator(f"{dropdown_content} [role='menuitem']").nth(0).locator('a')
-        
+    first_item_link = (
+        page.locator(f"{dropdown_content} [role='menuitem']").nth(0).locator("a")
+    )
+
     # 6001-MARK-059
-    expected_href = r"^https:\/\/governance\.stagnet1\.vega\.rocks\/proposals\/[a-f0-9]{64}$"
-    assert first_item_link.inner_text() == 'View proposal'    
-    assert re.match(expected_href, first_item_link.get_attribute('href'))
-  
-    # temporary skip 
+    expected_href = (
+        r"^https:\/\/governance\.stagnet1\.vega\.rocks\/proposals\/[a-f0-9]{64}$"
+    )
+    assert first_item_link.inner_text() == "View proposal"
+    assert re.match(expected_href, first_item_link.get_attribute("href"))
+
+    # temporary skip
     # 6001-MARK-060
     # proposed_markets_tab = page.get_by_test_id("tab-proposed-markets")
     # external_links = proposed_markets_tab.locator("font-alpha")
     # last_link = external_links.last
     # assert last_link.inner_text() == 'Propose a new market'
-        
+
     # expected_href = f"https://governance.stagnet1.vega.rocks/proposals/propose/new-market"
     # assert last_link.get_attribute('href') == expected_href
 
-@pytest.mark.usefixtures("proposed_market", "risk_accepted")
-def test_can_drag_and_drop_columns(proposed_market, vega: VegaService, page: Page):
-    # 6001-MARK-063
-    # setup market in proposed step, without liquidity provided
-    market_id = proposed_market
-    page.goto(f"/#/markets/{market_id}")
 
-    # approve market
-    governance.approve_proposal(
-        key_name=MM_WALLET.name,
-        proposal_id=market_id,
-        wallet=vega.wallet,
-    )
-    page.goto(f"/#/markets/all")
+@pytest.mark.usefixtures("risk_accepted")
+def test_can_drag_and_drop_columns(proposed_market, page: Page):
+    # 6001-MARK-063
+    page.goto("/#/markets/all")
     page.click('[data-testid="Proposed markets"]')
     col_market = page.locator('[col-id="market"]').first
     col_vote = page.locator('[col-id="voting"]').first
     col_market.drag_to(col_vote)
 
     # Check the attribute of the dragged element
-    attribute_value = col_market.get_attribute('aria-colindex')
-    assert attribute_value != '1'
-
-@pytest.mark.usefixtures("simple_market", "risk_accepted")
-def test_can_see_no_markets_message(simple_market, vega: VegaService, page: Page):    
-    page.goto(f"/#/markets/all")
-    page.get_by_test_id("Proposed markets").click()
-
-    # 6001-MARK-061
-    tab_proposed_markets = page.locator('[data-testid="tab-proposed-markets"]')
-    assert 'No proposed markets' in tab_proposed_markets.text_content()
+    attribute_value = col_market.get_attribute("aria-colindex")
+    assert attribute_value != "1"

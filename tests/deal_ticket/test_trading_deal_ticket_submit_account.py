47c56fb88ec47663d2d7cddb6dce2d930a6d742e
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-10-04 17:08:22 +0200

    rework

--- tests/deal_ticket/test_trading_deal_ticket_submit_account.py
+++ tests/deal_ticket/test_trading_deal_ticket_submit_account.py
@@ -2,6 +2,8 @@ import pytest
 from playwright.sync_api import Page, expect
 from vega_sim.service import VegaService
 from actions.utils import wait_for_toast_confirmation
+from conftest import init_vega
+from fixtures.market import setup_continuous_market
 
 
 
@@ -11,11 +13,21 @@ place_order = "place-order"
 deal_ticket_warning_margin = "deal-ticket-warning-margin"
 deal_ticket_deposit_dialog_button = "deal-ticket-deposit-dialog-button"
 
+@pytest.fixture(scope="module")
+def vega(request):
+    with init_vega(request) as vega:
+        yield vega
+
+
+@pytest.fixture(scope="module")
+def continuous_market(vega):
+    return setup_continuous_market(vega)
+
 @pytest.mark.usefixtures("page", "auth", "risk_accepted")
 def test_should_display_info_and_button_for_deposit(continuous_market, vega: VegaService, page: Page):
     page.goto(f"/#/markets/{continuous_market}")
-    page.get_by_test_id(order_size).type("200000")
-    page.get_by_test_id(order_price).type("20")
+    page.get_by_test_id(order_size).fill("200000")
+    page.get_by_test_id(order_price).fill("20")
     # 7002-SORD-060
     expect(page.get_by_test_id(deal_ticket_warning_margin)).to_have_text("You may not have enough margin available to open this position.")
     page.get_by_test_id(deal_ticket_warning_margin).hover()
@@ -30,8 +42,8 @@ def test_should_show_an_error_if_your_balance_is_zero(continuous_market, vega: V
     page.get_by_test_id("manage-vega-wallet").click()
     page.locator('[role="menuitemradio"]').nth(4).click()
     page.reload()
-    page.get_by_test_id(order_size).type("200")
-    page.get_by_test_id(order_price).type("20")
+    page.get_by_test_id(order_size).fill("200")
+    page.get_by_test_id(order_price).fill("20")
     # 7002-SORD-060
     page.get_by_test_id(place_order).is_enabled()
     # 7002-SORD-003

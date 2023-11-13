bc331298dc4d84569c40844907611fda15eb88bc
bwallacee <ben@vega.xyz>
2023-11-13 12:27:42 +0000

    chore: pr comments

--- tests/liquidity_provision/test_liquidity_provision.py
+++ tests/liquidity_provision/test_liquidity_provision.py
@@ -21,11 +21,13 @@ def test_liquidity_provision_amendment(continuous_market, vega: VegaService, pag
     # TODO Refactor asserting the grid
     page.goto(f"/#/liquidity/{continuous_market}")
     page.get_by_test_id("manage-vega-wallet").click()
+    #TODO Rename "mm" to "marketMaker" so that we don't have to specify where to click when switching wallets
+    # Currently the default click will click the middle of the element which will click the copy wallet key button
     page.locator('[role="menuitemradio"] >> .mr-2.uppercase').nth(1).click(position={ "x": 0, "y": 0}, force=True)
     page.reload()
     row = page.get_by_test_id("tab-myLP").locator(".ag-center-cols-container .ag-row").first
     expect(row).to_contain_text(
-        "Active10,000.0010,000.000.00%10,000.00100.00%10,000.00-100.00%0.00%---5/9/2023,"
+        "Active10,000.0010,000.000.00%10,000.00100.00%10,000.00-100.00%0.00%"
     )
     vega.submit_simple_liquidity(
         key_name="mm",
@@ -40,11 +42,11 @@ def test_liquidity_provision_amendment(continuous_market, vega: VegaService, pag
     page.reload()
     row = page.get_by_test_id("tab-myLP").locator(".ag-center-cols-container .ag-row").first
     expect(row).to_contain_text(
-        "Updating next epoch10,000.00 (100.00)10,000.00 (100.00)0.00% (0.10%)10,000.00100.00%10,000.00-100.00%100.00%---5/9/2023,"
+        "Updating next epoch10,000.00 (100.00)10,000.00 (100.00)0.00% (0.10%)10,000.00100.00%10,000.00-100.00%100.00%"
     )
     next_epoch(vega=vega)
     page.reload()
     row = page.get_by_test_id("tab-myLP").locator(".ag-center-cols-container .ag-row").first
     expect(row).to_contain_text(
-        "Active100.00100.000.10%100.00100.00%100.00-100.00%0.00%100.00%0.00%0.00%5/9/2023,"
+        "Active100.00100.000.10%100.00100.00%100.00-100.00%0.00%100.00%0.00%0.00%"
     )
